b64d9b3179bb4f369673763c277d2c29f34689a6
jeffzhou <616744200@qq.com>
2022-05-26 10:55:41 +0800

    update

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"maotai/model/purchase"
 	"maotai/utils"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -17,11 +18,61 @@ const (
 var (
 	startTime  = time.Now()
 	pushTime   = startTime
-	contentMap = make(map[string]struct{})
+	cookie     string
+	httpClient *utils.Request
 )
 
+func init() {
+	_, cookie = readCookie()
+	if cookie == "" {
+		os.Exit(0)
+	}
+
+	httpClient = utils.NewHttpClient()
+}
+
 func main() {
-	httpClient := utils.NewHttpClient()
+	for {
+		contentChan := make(chan purchase.Purchase, 100)
+		wg := sync.WaitGroup{}
+		for i, v := range purchase.GetShopPurchaseBodyList() {
+			wg.Add(1)
+			go func(i int, v purchase.PurchaseBody, contentChan chan purchase.Purchase) {
+				err, res := httpClient.WithHeaders(map[string]interface{}{
+					"Cookie": cookie,
+				}).PostJson(HOST, v)
+
+				if err != nil {
+					fmt.Errorf("请求异常： %s", err.Error())
+				}
+				p := purchase.Purchase{}
+				json.Unmarshal(res, &p)
+				wg.Done()
+				contentChan <- p
+				//fmt.Printf("%+v \n", p)
+			}(i, v, contentChan)
+		}
+		wg.Wait()
+		close(contentChan)
+
+		var content string
+		var i int
+		var stockSum int
+		for p := range contentChan {
+			i++
+			msg := fmt.Sprintf("- 门店%d：%s \t 库存：%d \t \n", i, p.Data.ShopInfo.ShopName, p.Data.PurchaseInfo.Inventory)
+			fmt.Print(msg)
+			content += msg
+			stockSum += p.Data.PurchaseInfo.Inventory
+		}
+		if stockSum > 0 {
+			push(content)
+		}
+		time.Sleep(time.Second * 5)
+	}
+}
+
+func readCookie() (err error, cookie string) {
 	file, err := os.Open("cookie.txt")
 	if err != nil {
 		fmt.Errorf("open cookie file error: %s", err.Error())
@@ -32,43 +83,13 @@ func main() {
 		fmt.Errorf("read cookie file error: %s", err.Error())
 		return
 	}
-	cookie := string(fileContent)
-	for {
-		go func() {
-			var content string
-			var hasStock bool
-			for i, v := range purchase.GetShopPurchaseBodyList() {
-				err, res := httpClient.WithHeaders(map[string]interface{}{
-					"Cookie": cookie,
-				}).PostJson(HOST, v)
-
-				if err != nil {
-					fmt.Errorf("请求异常： %s", err.Error())
-				}
-				p := purchase.Purchase{}
-				json.Unmarshal(res, &p)
-
-				//fmt.Printf("%+v \n", p)
-				msg := fmt.Sprintf("- 门店%d：%s \t 库存：%d \t \n", i+1, p.Data.ShopInfo.ShopName, p.Data.PurchaseInfo.Inventory)
-				fmt.Print(msg)
-				content += msg
-				if p.Data.PurchaseInfo.Inventory > 0 {
-					hasStock = true
-				}
-			}
-			if !hasStock {
-				push(content)
-			}
-		}()
-		time.Sleep(time.Second * 10)
-	}
+	cookie = string(fileContent)
+	return
 }
 
 func push(content string) {
-	_, ok := contentMap[content]
-	if pushTime.Add(10*time.Second).Before(time.Now()) && !ok {
+	if pushTime.Add(time.Hour).Before(time.Now()) {
 		utils.New().Send(content)
 		pushTime = time.Now()
-		contentMap[content] = struct{}{}
 	}
 }
